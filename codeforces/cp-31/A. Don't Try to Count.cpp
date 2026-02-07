c72414bb60e15d49a237e3120e848f86a2e3f8ff
Cadenviv07 <cva701281@gmail.com>
2026-02-07 01:16:54 -0500

    competitive programming

--- codeforces/cp-31/A. Don't Try to Count.cpp
+++ codeforces/cp-31/A. Don't Try to Count.cpp
@@ -1,11 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-     
     
-
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++){
+        int n,m;
+        cin >> n >> m;
+        string x;
+        cin >> x;
+        string s;
+        cin >> s;
+        bool found = false;        
+        for(int i = 0; i <= 5; i++){
+            if(x.find(s) != string::npos){
+                cout << i << endl;
+                found = true;
+                break;
+            }
+            x+=x;
+        }
+        if(!found){
+            if(x.find(s) != string::npos){
+                cout << 6 << endl;
+            }else{
+                cout << -1 << endl;
+            }
+        }
+    }
     return 0;
 }
--- codeforces/cp-31/A. How Much Does Daytona Cost_.cpp
+++ codeforces/cp-31/A. How Much Does Daytona Cost_.cpp
@@ -5,7 +5,28 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    
+    int t;
+    cin >> t;
+
+    for(int i = 0; i < t; i++){
+        int n, k;
+        cin >> n >> k;
+        bool b = false;
+        for(int j = 0; j < n; j++){
+            int c;
+            cin >> c;
+            if(c == k){
+                b = true;
+            }
+        }
+
+        if(b){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
+        }
+    }
+            
 
     return 0;
 }
